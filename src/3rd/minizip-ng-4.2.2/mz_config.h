1748f99dcc50ad067a0bcd404a1b8d442595ebcc
friendan <407583729@qq.com>
2026-08-14 20:17:18 +0800

    update

---
+++ src/3rd/minizip-ng-4.2.2/mz_config.h
@@ -0,0 +1,15 @@
+#ifndef MZ_CONFIG_H
+#define MZ_CONFIG_H
+
+/* Hand-maintained for DuiLib Windows static build (no configure_file). */
+
+#define HAVE_DIRENT_H 0
+#define HAVE_SYS_DIRENT_H 0
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_PDIR 0
+#define HAVE_FSEEKO 0
+#define HAVE_SYMLINK 0
+#define HAVE_READLINK 0
+
+#endif
